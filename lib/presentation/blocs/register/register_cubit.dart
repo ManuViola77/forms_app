058bde4206905311095e2d54d6d55869edb5da79
Manuela Viola <manuela.viola@gmail.com>
2025-05-30 16:02:48 -0300

    feat: create register cubit

---
+++ lib/presentation/blocs/register/register_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterFormState> {
+  RegisterCubit() : super(const RegisterFormState());
+
+  void onSubmit() {
+    print('Submit: $state');
+  }
+
+  void onUsernameChanged(String username) {
+    emit(state.copyWith(username: username));
+  }
+
+  void onEmailChanged(String email) {
+    emit(state.copyWith(email: email));
+  }
+
+  void onPasswordChanged(String password) {
+    emit(state.copyWith(password: password));
+  }
+
+  void onFormSubmitted() {
+    emit(state.copyWith(formStatus: FormStatus.validating));
+  }
+}
